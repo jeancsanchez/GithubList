f2e355f61903bb2f8ac86fc1f6716c1343be1478
jeancsanchez <jeancs.patricio@gmail.com>
2017-12-28 11:15:22 -0300

    Showing user details.

--- presentation/src/main/java/com/github/jeancarlos/githublist/features/detail/DetailActivity.java
+++ presentation/src/main/java/com/github/jeancarlos/githublist/features/detail/DetailActivity.java
@@ -4,16 +4,23 @@ package com.github.jeancarlos.githublist.features.detail;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.jeancarlos.githublist.R;
 import com.github.jeancarlos.githublist.base.mvp.BaseActivity;
 import com.github.jeancarlos.githublist.domain.model.GithubRepo;
 import com.github.jeancarlos.githublist.domain.model.User;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
 import javax.inject.Inject;
+
+import butterknife.BindView;
 
 /**
  * This class represents the User details screen.
@@ -29,6 +36,38 @@ public class DetailActivity extends BaseActivity implements DetailContract.View 
 
     @Inject
     DetailContract.Presenter presenter;
+
+    @Nullable
+    @BindView(R.id.imgDetailUser)
+    ImageView mImgUserPicture;
+
+    @Nullable
+    @BindView(R.id.txtDetailName)
+    TextView mTxtUserName;
+
+    @Nullable
+    @BindView(R.id.txtDetailNickname)
+    TextView mTxtUserNickname;
+
+    @Nullable
+    @BindView(R.id.txtDetailBio)
+    TextView mTxtUserBio;
+
+    @Nullable
+    @BindView(R.id.txtDetailRepos)
+    TextView mTxtReposCount;
+
+    @Nullable
+    @BindView(R.id.txtDetailGists)
+    TextView mTxtGistsCount;
+
+    @Nullable
+    @BindView(R.id.txtDetailStars)
+    TextView mTxtStarsCount;
+
+    @Nullable
+    @BindView(R.id.txtDetailFollowers)
+    TextView mTxtFollowersCount;
 
     /**
      * Gets a new intent for this class.
@@ -47,6 +86,10 @@ public class DetailActivity extends BaseActivity implements DetailContract.View 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
 
         getAppComponent()
                 .activityComponent()
@@ -76,10 +119,48 @@ public class DetailActivity extends BaseActivity implements DetailContract.View 
         }
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+
+        return true;
+    }
 
     @Override
     public void showUserDetails(User user) {
+        if (mTxtUserName != null) {
+            mTxtUserName.setText(user.getName());
+        }
 
+        if (mTxtUserNickname != null) {
+            mTxtUserNickname.setText(getString(R.string.item_user_nick, user.getLogin()));
+        }
+
+        if (mImgUserPicture != null && user.getAvatarUrl() != null) {
+            Picasso.with(this).load(user.getAvatarUrl()).into(mImgUserPicture);
+        }
+
+        if (mTxtReposCount != null) {
+            mTxtReposCount.setText(String.valueOf(user.getPublicRepos()));
+        }
+
+        if (mTxtGistsCount != null) {
+            mTxtGistsCount.setText(String.valueOf(user.getPublicGists()));
+        }
+
+        if (mTxtStarsCount != null) {
+            mTxtStarsCount.setText(String.valueOf(user.getFollowing()));
+        }
+
+        if (mTxtFollowersCount != null) {
+            mTxtFollowersCount.setText(String.valueOf(user.getFollowers()));
+        }
+
+        if (mTxtUserBio != null) {
+            mTxtUserBio.setText(user.getBio());
+        }
     }
 
     @Override
